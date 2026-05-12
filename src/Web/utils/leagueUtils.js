ec1ffef48e289464383e9735e4438a40953b87cc
David Catanzariti <supermakario456@gmail.com>
2026-05-12 14:43:10 -0400

    added leagueUtils

---
+++ src/Web/utils/leagueUtils.js
@@ -0,0 +1,19 @@
+export var league = {
+    "Team_1" : {
+        "owner" : "team1",
+        "roster" : []
+    },
+    "Team_2" : {
+        "owner" : "team2",
+        "roster" : []
+    },
+    "Team_3" : {
+        "owner" : "team3",
+        "roster" : []
+    },
+    "Team_4" : {
+        "owner" : "team4",
+        "roster" : []
+    },
+}
+
