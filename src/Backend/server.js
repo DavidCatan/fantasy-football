b51f91341616a0adbe733f43e239f31e6e22a40f
David Catanzariti <supermakario456@gmail.com>
2026-09-14 22:39:29 -0400

    added environment variable support for port

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -13,6 +13,7 @@ import cron from 'node-cron';
 
 
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({server});
@@ -1495,4 +1496,4 @@ function broadcastUpdate(type, data, league_id){
     
 }
 
-server.listen(3000, () => console.log('Backend running on port 3000'));
+server.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
